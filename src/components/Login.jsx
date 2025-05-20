8df169cd3f11f25a32b9a365448a66a78893bfd8
BastiEbel <sebastianebel@hotmail.de>
2025-05-20 21:20:40 +0200

    begin with authentication with github profil

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -7,25 +7,22 @@ export default function Login() {
   if (session) {
     return (
       <div>
-        <p>Signed in as {session.user.name}</p>
+        <p>Angemeldet als {session.user.name}</p>
         <button
           className="group underline rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
           onClick={() => signOut()}
         >
-          Sign out
+          Abmelden
         </button>
       </div>
     );
   }
   return (
-    <>
-      Not signed in <br />
-      <button
-        className="group underline rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-        onClick={() => signIn("github")}
-      >
-        Sign in
-      </button>
-    </>
+    <button
+      className="group underline rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+      onClick={() => signIn("github")}
+    >
+      Mit GitHub anmelden
+    </button>
   );
 }

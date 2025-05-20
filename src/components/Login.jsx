2889a01d2de1df26a2b312db3f5a6ca8026414ee
GreenfieldC <101995422+GreenfieldC@users.noreply.github.com>
2025-05-20 17:35:10 +0200

    feat(Github): Anmeldung nun mit Github-Konto möglich
    Anleitung für GithubID etc:
    1. https://github.com/settings/developers verwenden für nötige Daten
    2. .env.local - Datei erstellen und Datein dort rein
    
    Es ist nun möglich, sich mittels des eigenen Github-Kontos in die App anzumelden.
    
    #1

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -20,7 +20,7 @@ export default function Login() {
       Not signed in <br />
       <button
         className="group underline rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-        onClick={() => signIn()}
+        onClick={() => signIn("github")}
       >
         Sign in
       </button>

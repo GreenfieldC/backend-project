b82477cf0d403ffdf6390d66e348f8c482d41740
BastiEbel <sebastianebel@hotmail.de>
2025-05-22 19:38:12 +0200

    change styling and set button for recipes visible

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -7,22 +7,25 @@ export default function Login() {
   if (session) {
     return (
       <div>
-        <p>Angemeldet als {session.user.name}</p>
+        <p>Signed in as {session.user.name}</p>
         <button
           className="group underline rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
           onClick={() => signOut()}
         >
-          Abmelden
+          Sign out
         </button>
       </div>
     );
   }
   return (
-    <button
-      className="group underline rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-      onClick={() => signIn("github")}
-    >
-      Mit GitHub anmelden
-    </button>
+    <>
+      Not signed in <br />
+      <button
+        className="group underline rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+        onClick={() => signIn("github")}
+      >
+        Sign in
+      </button>
+    </>
   );
 }

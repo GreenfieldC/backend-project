80be546958fd330a2c864cfee77d36bd98a7f004
BastiEbel <sebastianebel@hotmail.de>
2025-05-22 19:38:12 +0200

    change styling and set button for recipes visible

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,10 @@
+"use client";
+import { useSession } from "next-auth/react";
 import Login from "@/components/Login";
 import Link from "next/link";
 
 export default function Home() {
+  const { data: session } = useSession();
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-br from-blue-100 via-white to-purple-200 dark:from-gray-900 dark:via-gray-800 dark:to-gray-900 transition-colors duration-500">
       <header className="flex flex-col items-center gap-4 mb-12">
@@ -10,28 +13,30 @@ export default function Home() {
         </h1>
         <Login />
       </header>
-      <Link
-        className="group underline rounded-xl border border-gray-200 dark:border-gray-700 px-8 py-4 text-lg font-medium shadow-md bg-white/80 dark:bg-gray-800/80 hover:bg-blue-100 hover:dark:bg-blue-900/40 transition-colors duration-300"
-        href="/recipes"
-      >
-        <span className="flex items-center gap-2">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth={1.5}
-            stroke="currentColor"
-            className="w-6 h-6"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M4.5 12.75l6 6 9-13.5"
-            />
-          </svg>
-          Recipes ansehen
-        </span>
-      </Link>
+      {session && (
+        <Link
+          className="group underline rounded-xl border border-gray-200 dark:border-gray-700 px-8 py-4 text-lg font-medium shadow-md bg-white/80 dark:bg-gray-800/80 hover:bg-blue-100 hover:dark:bg-blue-900/40 transition-colors duration-300"
+          href="/recipes"
+        >
+          <span className="flex items-center gap-2">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-6 h-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M4.5 12.75l6 6 9-13.5"
+              />
+            </svg>
+            Recipes ansehen
+          </span>
+        </Link>
+      )}
     </main>
   );
 }

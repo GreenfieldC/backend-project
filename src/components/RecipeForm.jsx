a1ecdc35324afe047e42202fed3535e3974e0ffa
BastiEbel <sebastianebel@hotmail.de>
2025-06-10 18:36:53 +0200

    formatted later

--- src/components/RecipeForm.jsx
+++ src/components/RecipeForm.jsx
@@ -80,20 +80,24 @@ export default function RecipeForm({
             .split('\n')
             .filter(Boolean)
         : [],
-      ingredients: ingredientAmount.filter(
-        (ingredient, index) =>
-          formData.get(`name-${index}`)?.trim() ||
-          formData.get(`amount-${index}`)?.trim() ||
-          formData.get(`units-${index}`)?.trim()
-      ).map(
-        (ingredient, index) => ({
+      ingredients: ingredientAmount
+        .filter(
+          (ingredient, index) =>
+            formData
+              .get(`name-${index}`)
+              ?.trim() ||
+            formData
+              .get(`amount-${index}`)
+              ?.trim() ||
+            formData.get(`units-${index}`)?.trim()
+        )
+        .map((ingredient, index) => ({
           name: formData.get(`name-${index}`),
           amount: parseFloat(
             formData.get(`amount-${index}`)
           ),
           units: formData.get(`units-${index}`)
-        })
-      )
+        }))
     });
     if (!isEdit) {
       setName('');
